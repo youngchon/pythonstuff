5aef4328b07273f93923e837bafaec44e401ec6b
youngchon <young.chon@email.wsu.edu>
2014-11-17 14:25:55 -0800

    file error catching

--- extrahop/countem.py
+++ extrahop/countem.py
@@ -23,36 +23,39 @@ import sys
 def countem(file):
     numlines = 0
     multiline = False
+    try:
+        with open(file, 'r') as file:
+            for line in file:
+                toks = line.split()
+                if toks != []:
+                    if multiline == False:
+                        #grab first token
+                        cur = toks[0]
+                        print(len(cur))
+                        if len(cur) >= 2:
+                            if cur[0] != '/' and cur[1] != '/':
+                                numlines += 1 
+                            elif cur[0] =='/' and cur[1] == '*': # how to deal with the end of this
+                                #check to see if the last char is terminted with a /
+                                multiline = True
+                                if cur[-1] == '/':
+                                    if cur[-2] == '*':
+                                        multiline = False
+                        ## now lets check to see if anywhere inbetween some one decided
+                        ## to start a code block
+                        for token in toks[1:]: #dont recount hte first one
+                        	if len(token) >= 2:
+                        		if token[0] == '/' and token[1] == '*':
+                        			multiline = True
+                    else:
+                        #multilined grab last token
+                        cur = toks[-1]
+                        if cur[-1] == '/' and cur[-2] == '*':
+                            multiline = False
+            print ("number of lines: " + str(numlines))
+    except:
+        print("Unable to open file")
 
-    with open(file, 'r') as file:
-        for line in file:
-            toks = line.split()
-            if toks != []:
-                if multiline == False:
-                    #grab first token
-                    cur = toks[0]
-                    print(len(cur))
-                    if len(cur) >= 2:
-                        if cur[0] != '/' and cur[1] != '/':
-                            numlines += 1 
-                        elif cur[0] =='/' and cur[1] == '*': # how to deal with the end of this
-                            #check to see if the last char is terminted with a /
-                            multiline = True
-                            if cur[-1] == '/':
-                                if cur[-2] == '*':
-                                    multiline = False
-                    ## now lets check to see if anywhere inbetween some one decided
-                    ## to start a code block
-                    for token in toks[1:]: #dont recount hte first one
-                    	if len(token) >= 2:
-                    		if token[0] == '/' and token[1] == '*':
-                    			multiline = True
-                else:
-                    #multilined grab last token
-                    cur = toks[-1]
-                    if cur[-1] == '/' and cur[-2] == '*':
-                        multiline = False
-        print ("number of lines: " + str(numlines))
 
 if __name__ == '__main__':
     file = sys.argv[1]
